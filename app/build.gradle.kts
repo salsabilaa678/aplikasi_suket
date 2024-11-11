56c79a98e9dc47e4d0b9e437e1c8f62f4ae0cd65
caca <salsaanindyaputri7655@gmail.com>
2024-11-11 22:15:14 +0700

    android manifest permission

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.gson)
+    implementation(libs.retrofit.scalars)
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging)
 }
--- app/src/main/java/com/example/pemrograman_bergerak/Formuliraktif2Activity.kt
+++ app/src/main/java/com/example/pemrograman_bergerak/Formuliraktif2Activity.kt
@@ -5,29 +5,50 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class Formuliraktif2Activity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_formuliraktif2)
 
         val daftarButton1 = findViewById<Button>(R.id.button5o)
+        val imageButton1 = findViewById<ImageButton>(R.id.back)
+        val jelajahiButton1 = findViewById<Button>(R.id.button6)
+        val jelajahiButton2 = findViewById<Button>(R.id.button9)
+        val jelajahiButton3 = findViewById<Button>(R.id.button10)
 
+        // Intent for button5o to move to Formuliraktif3Activity
         daftarButton1.setOnClickListener {
             val intent = Intent(this@Formuliraktif2Activity, Formuliraktif3Activity::class.java)
             startActivity(intent)
         }
-        val imageButton1 = findViewById<ImageButton>(R.id.back)
 
+        // Intent for back button to move back to FormuliraktifActivity
         imageButton1.setOnClickListener {
             val intent = Intent(this@Formuliraktif2Activity, FormuliraktifActivity::class.java)
             startActivity(intent)
         }
+
+        // Intent for "Jelajahi" buttons to open gallery
+        jelajahiButton1.setOnClickListener {
+            openGallery()
+        }
+
+        jelajahiButton2.setOnClickListener {
+            openGallery()
+        }
+
+        jelajahiButton3.setOnClickListener {
+            openGallery()
+        }
+    }
+
+    private fun openGallery() {
+        // Code to open gallery for selecting images
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*" // Restricting selection to images
+        startActivityForResult(intent, 100) // Request code 100 for identifying result later
     }
 }
